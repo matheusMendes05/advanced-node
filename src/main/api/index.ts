31ed83cffdb6c1afaebbbeb9529bf7341d083458
Matheus Mendes <matheus_mendes05@hotmail.com>
2023-11-30 12:19:56 -0300

    add eslint

--- src/main/api/index.ts
+++ src/main/api/index.ts
@@ -1,6 +1,6 @@
 import '../config/module-alias'
-import { PersonController } from "@/application/controllers";
+import { PersonController } from '@/application/controllers'
 
-const p = new PersonController();
-console.log(p.speak('rodrigo'));
+const p = new PersonController()
+console.log(p.speak('rodrigo'))
 p.speak()
