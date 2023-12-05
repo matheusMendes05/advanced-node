205a1a8ce6849ac6a3488b495a2e65b795586504
matheus_mendes5 <Matheus.Lima@3pbrasil.com.br>
2023-12-05 19:17:09 -0300

    make JwtTokenGenerator implements TokenGenerator AxiosHttpClien implement HttpGetClient

--- src/infra/crypto/jwt-token-generator.ts
+++ src/infra/crypto/jwt-token-generator.ts
@@ -1,10 +1,12 @@
 import { TokenGenerator } from '@/data/contracts/crypto'
 import jwt from 'jsonwebtoken'
 
-export class JsonTokenGenerator {
+export class JsonTokenGenerator implements TokenGenerator {
   constructor (private readonly secret: string) {}
   async generateToken (params: TokenGenerator.Params): Promise<TokenGenerator.Result> {
     const expirationInSeconds = params.expirationInMs / 1000
-    return jwt.sign({ key: params.key }, this.secret, { expiresIn: expirationInSeconds })
+    return jwt.sign({ key: params.key }, this.secret, {
+      expiresIn: expirationInSeconds
+    })
   }
 }
