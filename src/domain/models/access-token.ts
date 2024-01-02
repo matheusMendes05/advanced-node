27fc3dc1801ede6a86fa3548829eb918024b5147
Matheus Mendes <matheus_mendes05@hotmail.com>
2024-01-02 11:52:12 -0300

    ensure FacebookLoginController returns 200 authentication succeds

--- src/domain/models/access-token.ts
+++ src/domain/models/access-token.ts
@@ -1,5 +1,5 @@
 export class AccessToken {
-  constructor (private readonly value: string) {}
+  constructor (readonly value: string) {}
 
   static get expirationInMS (): number {
     return 30 * 60 * 1000
